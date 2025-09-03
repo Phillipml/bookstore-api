6ad27a51f35ea9d83ea23abd10f6c35c5389c583
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-03 18:33:29 -0300

    test: Teste CategoryViewset editado

--- product/tests/test_category_viewset.py
+++ product/tests/test_category_viewset.py
@@ -1,27 +1,17 @@
 import json
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
 from django.urls import reverse
-from rest_framework import status
+
 from product.models import Category
 from product.factories import CategoryFactory
 
 
-class CategoryViewSet(TestCase):
+class CategoryViewSet(APITestCase):
+    client = APIClient()
+
     def setUp(self):
-        self.category = CategoryFactory()
-
-    def test_create_category(self):
-        data = json.dumps({"title": "terror", "description": "categoria de terror"})
-
-        response = self.client.post(
-            reverse("category-list", kwargs={"version": "v1"}),
-            data,
-            content_type="application/json",
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        created_category = Category.objects.get(id=response.data["id"])
-        self.assertEqual(created_category.title, "terror")
+        self.category = CategoryFactory(title="books")
 
     def test_get_all_category(self):
         response = self.client.get(reverse("category-list", kwargs={"version": "v1"}))
@@ -29,3 +19,16 @@ class CategoryViewSet(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         category_data = json.loads(response.content)
         self.assertEqual(category_data[0]["title"], self.category.title)
+
+    def test_create_category(self):
+        data = json.dumps({"title": "suspense"})
+
+        response = self.client.post(
+            reverse("category-list", kwargs={"version": "v1"}),
+            data=data,
+            content_type="application/json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        created_category = Category.objects.get(title="suspense")
+        self.assertEqual(created_category.title, "suspense")
--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -1,5 +1,4 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
 
 from product.models import Category
 from product.serializers.category_serializers import CategorySerializer
@@ -8,4 +7,3 @@ from product.serializers.category_serializers import CategorySerializer
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-    permission_classes = [AllowAny]
