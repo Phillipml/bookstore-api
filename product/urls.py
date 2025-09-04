51f14d7eb7c44e43af16769a65a2e8504ce85605
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-04 15:16:27 -0300

    feat: Token Authenticator incluido

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from rest_framework import routers
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 from product import viewsets
 
@@ -8,4 +7,4 @@ router = routers.SimpleRouter()
 router.register(r"product", viewsets.ProductViewSet, basename="product")
 router.register(r"category", viewsets.CategoryViewSet, basename="category")
 
-urlpatterns = [path("", include(router.urls))] + debug_toolbar_urls()
+urlpatterns = [path("", include(router.urls))]
