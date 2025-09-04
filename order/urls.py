23ec07c205eff673e1ec133a08c9f403bd96436d
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-04 10:56:04 -0300

    chore: django-toolbar adicionado

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from order import viewsets
 
 router = routers.SimpleRouter()
 router.register(r"order", viewsets.OrderViewSet, basename="order")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls))] + debug_toolbar_urls()
--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from product import viewsets
 
@@ -7,4 +8,4 @@ router = routers.SimpleRouter()
 router.register(r"product", viewsets.ProductViewSet, basename="product")
 router.register(r"category", viewsets.CategoryViewSet, basename="category")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls))] + debug_toolbar_urls()
