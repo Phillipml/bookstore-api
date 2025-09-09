54b0316a1c19065fbaf8e975b178814c49a8ebb0
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-09 17:18:09 -0300

    code: formatted

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "bookstore.urls"
