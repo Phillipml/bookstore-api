90e2ec7209c443d64672d2976ee42336254ece94
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-09 14:07:28 -0300

    ready to pythonanywhere

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,12 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from product.models import Product
 from product.serializers.product_serializers import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = ProductSerializer
 
     def get_queryset(self):
