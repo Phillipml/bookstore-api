f3ceccb37528eb29e18769b0e8ce881d7fc03a2c
Phillipml <contato.phillip.menezes@gmail.com>
2025-09-09 17:41:41 -0300

    feat: autodeploy with python-anywhere and url / route fixed

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,10 +2,26 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 import debug_toolbar.toolbar
 from rest_framework.authtoken.views import obtain_auth_token
 
+def home(request):
+    return JsonResponse({
+        "message": "Bookstore API",
+        "version": "1.0.0",
+        "endpoints": {
+            "products": "/bookstore/v1/product/",
+            "categories": "/bookstore/v1/category/",
+            "orders": "/bookstore/v1/order/",
+            "admin": "/admin/",
+            "api_token": "/api-token-auth/"
+        },
+        "documentation": "Use the browsable API interface for testing"
+    })
+
 urlpatterns = [
+    path("", home, name="home"),
     path("__debug__", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
     re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
